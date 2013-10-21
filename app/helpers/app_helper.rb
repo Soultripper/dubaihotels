9dc72c1a8060b9c51be4d721e9b38352efda6758
soultripper <sky@filmlovers.co.uk>
2013-10-21 10:22:19 +0100

    added city names

---
+++ app/helpers/app_helper.rb
@@ -0,0 +1,129 @@
+module AppHelper
+
+  def north_america
+    [
+      'New York',
+      'Las Vegas',
+      'San Francisco',
+      'Los Angeles',
+      'Chicago',
+      'San Diego',
+      'Washington',
+      'Orlando',
+      'Toronto',
+      'Montreal',
+      'Houston',
+      'New Orleans',
+      'Seattle',
+      'Vancouver',
+      'Cancun',
+      'San Antonio',
+      'Miami',
+      'Mexico City',
+      'Playa del Carmen',
+      'Boston'
+    ]
+  end
+
+  def south_america
+    [
+      'Buenos Aires',
+      'Rio de Janeiro',
+      'Lima',
+      'Cusco',
+      'Santiago',
+      'Bogota',
+      'Sao Paulo',
+      'Quito',
+      'Cartagena',
+      'Bariloche',
+      'Buzios',
+      'Salvador da Bahia',
+      'Arequipa',
+      'Florianopolis',
+      'Puerto Iguazu',
+      'La Paz',
+      'Paraty',
+      'Medellin',
+      'Mendoza',
+      'Porto Seguro'
+    ]
+  end
+
+  def europe
+    [
+      'London',
+      'Paris',
+      'Rome',
+      'Barcelona',
+      'Dublin',
+      'Madrid',
+      'Amsterdam',
+      'Prague',
+      'Moscow',
+      'Vienna',
+      'St. Petersburg',
+      'Venice',
+      'Warsaw',
+      'Dubrovnik',
+      'Benidorm',
+      'Berlin',
+      'Budapest',
+      'Munich',
+      'Milan',
+      'Florence',
+      'Lisbon'
+    ]
+  end
+
+  def middle_east_africa
+    [
+      'Dubai',
+      'Jerusalem',
+      'Tel Aviv',
+      'Abu Dhabi',
+      'Beirut',
+      'Anman',
+      'Petra',
+      'Doha',
+      'Manama',
+      'Riyadh',
+      'Damascus',
+      'Muscat',
+      'Marrakech',
+      'Cape Town',
+      'Cairo',
+      'Johannesburg',
+      'Sham El Sheikh',
+      'Fez',
+      'Luxor',
+      'Hurghada',
+      'Nairobi'
+    ]
+  end
+
+  def asia_pacific
+    [
+      'Bangkok',
+      'Bali',
+      'Beijing',
+      'Shanghai',
+      'Phuket',
+      'Kuala Lumpur',
+      'New Delhi',
+      'Tokyo',
+      'Singapore',
+      'Hanoi',
+      'Ho Chi Minh City',
+      'Siem Reap',
+      'Chiang Mai',
+      'Seoul',
+      'Guanzhou',
+      'Koh Samui',
+      'Hong Kong',
+      'Pattaya',
+      'Sydney',
+      'Melbourne'
+    ]
+  end
+end
--- app/helpers/hotels_helper.rb
+++ app/helpers/hotels_helper.rb
@@ -1,27 +1,4 @@
 module HotelsHelper
 
-  def nort_america
-    [
-      'New York',
-      'Las Vegas',
-      'San Francisco',
-      'Los Angeles',
-      'Chicago',
-      'San Diego',
-      'Washington',
-      'Orlando',
-      'Toronto',
-      'Montreal',
-      'Houston',
-      'New Orleans',
-      'Seattle',
-      'Vancouver',
-      'Cancun',
-      'San Antonio',
-      'Miami',
-      'Mexico City',
-      'Playa del Carmen',
-      'Boston'
-    ]
-  end
+
 end
