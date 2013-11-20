2ed9f3de99d392b295e4b328323f7fde6315ca5a
soultripper <sky@filmlovers.co.uk>
2013-11-20 16:46:32 +0000

    removed all redis ref

--- config/initializers/soulmate.rb
+++ config/initializers/soulmate.rb
@@ -1,6 +1,6 @@
 # config/initializers/soulmate.rb
 
-Soulmate.redis = "redis://rediscloud:HpZr1ty4p5ACGdNP@pub-redis-15018.eu-west-1-1.1.ec2.garantiadata.com:15018"
+# Soulmate.redis = "redis://rediscloud:HpZr1ty4p5ACGdNP@pub-redis-15018.eu-west-1-1.1.ec2.garantiadata.com:15018"
 
 # or you can asign an existing instance of Redis, Redis::Namespace, etc.
 # Soulmate.redis = $redis
