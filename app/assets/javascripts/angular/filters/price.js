5636e8f400ad91f6f7fa67a308084f21f24c0745
soultripper <sky@filmlovers.co.uk>
2014-01-07 18:14:31 +0000

    added cloak on results and replaced sold out to na

--- app/assets/javascripts/angular/filters/price.js
+++ app/assets/javascripts/angular/filters/price.js
@@ -1,6 +1,6 @@
 app.filter('price', function() {
   return function(value, symbol, precision) {
-    if(value <= 0 || value==undefined) return "sold out";
+    if(value <= 0 || value==undefined) return "N/A";
 
     return accounting.formatMoney(value, symbol, precision);
   };
