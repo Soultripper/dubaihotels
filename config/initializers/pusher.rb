10350e4e1d56df6b8c6bf860e06a9abbd9c986b1
soultripper <sky@filmlovers.co.uk>
2014-03-10 14:43:14 +0700

    config env for sidekiq

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -2,6 +2,8 @@ require 'pusher'
 
 if Rails.env=='development'
   Pusher.url = "http://e49cfdb8b8f62652d358:20dbcf2dcd4b6b382988@api-eu.pusher.com/apps/59660"
+elsif Rails.env == 'production'
+  Pusher.url = "http://e82d8d284afcc0071094:e1b815fe522a676a401f@api-eu.pusher.com/apps/68001"
 else
   Pusher.url = "http://1163c38d8c21940bd111:801d43e5a69c48a712c9@api-eu.pusher.com/apps/59659"
 end
