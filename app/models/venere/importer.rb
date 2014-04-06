61b284820fcd115a3febfdd706812975d2674c73
soultripper <sky@filmlovers.co.uk>
2014-04-06 23:00:26 +0700

    testing venere import

--- app/models/venere/importer.rb
+++ app/models/venere/importer.rb
@@ -13,23 +13,59 @@ class Venere::Importer
       #TODO: Update geography / nameaddress
     end
 
+    # def import(klass, filename=nil)
+
+    #   filename = get_and_store unless filename
+    #   filename = unzip filename
+
+    #   sql = sql_for klass
+
+    #   klass.delete_all
+    #   Log.debug "Deleted all #{klass} records"
+    #   Log.info sql
+    #   ActiveRecord::Base.connection.execute sql
+    # end
+
     def import(klass, filename=nil)
 
       filename = get_and_store unless filename
       filename = unzip filename
 
-      if klass.respond_to? :cols
-        sql = "copy #{klass.table_name} (#{klass.cols}) from '#{filename}' delimiter '|' CSV HEADER QUOTE '{' ESCAPE '^';"
-      else
-        sql = "copy #{klass.table_name} from '#{filename}' delimiter '|' CSV HEADER QUOTE '{' ESCAPE '^';"
-      end
-
+      data = File.open(filename) {|f| f.read }
 
       klass.delete_all
       Log.debug "Deleted all #{klass} records"
       Log.info sql
-      ActiveRecord::Base.connection.execute sql
+
+      conn = ActiveRecord::Base.connection_pool.checkout
+      raw  = conn.raw_connection
+      raw.exec(sql_for(klass))
+      data.each_line {|line| raw.put_copy_data(line) }
+      raw.put_copy_end
+
+      while res = raw.get_result do; end # very important to do this after a copy
+      ActiveRecord::Base.connection_pool.checkin(conn)
+      data = nil
+      filename
     end
+
+    def sql_for(klass)
+      if klass.respond_to? :cols
+         return "COPY #{klass.table_name} FROM STDIN DELIMITER '|' CSV HEADER QUOTE '{' ESCAPE '^';"
+      end
+        # return "copy #{klass.table_name} (#{klass.cols}) from '#{filename}' delimiter '|' CSV HEADER QUOTE '{' ESCAPE '^';" 
+      # "copy #{klass.table_name} from '#{filename}' delimiter '|' CSV HEADER QUOTE '{' ESCAPE '^';"
+      "COPY #{klass.table_name} FROM STDIN DELIMITER '|' CSV HEADER QUOTE '{' ESCAPE '^';"
+    end
+
+
+    # def self.up
+    #   c = Property.connection.raw_connection
+    #   c.exec(%q{COPY cities (city, state_id, lat, long) FROM STDIN})
+    #   data = File.open(File.dirname(__FILE__) + '/places_cities.pgdump') {|f| f.read }
+    #   data.each_line {|line| c.putline(line) }
+    #   c.endcopy
+    # end
 
     def get_and_store
       response = get_file
@@ -53,7 +89,7 @@ class Venere::Importer
     end
 
     def get_file
-
+      Log.debug "Downloading file at #{url}"
       conn = Faraday.new(url)
       conn.get
     end
@@ -65,11 +101,12 @@ class Venere::Importer
 
 
     def store_file(response)
-      tmp_file = Tempfile.new([Time.now.to_i, '.csv'], "#{Rails.root}/tmp")
+      tmp_file = Tempfile.new([Time.now.to_i, '.zip'], "#{Rails.root}/tmp")
       tmp_file.binmode
       tmp_file.write response.body
       tmp_file.close
       Log.info "Written temp file #{ tmp_file.path}"
+      response = nil
       tmp_file.path
     end
   end
--- lib/tasks/hotels.rake
+++ lib/tasks/hotels.rake
@@ -7,11 +7,4 @@ namespace :hotels do
     Venere::Importer.hotels
   end
 
-
-  def import_agoda_file(klass, filename)
-    sql =  "copy #{klass.table_name} (#{klass.cols}) from '#{Rails.root}/tmp/agoda/#{filename}.csv' with (FORMAT csv, DELIMITER ',', HEADER true, QUOTE '}')"    
-    Log.info sql
-    ActiveRecord::Base.connection.execute sql
-  end
-
 end
