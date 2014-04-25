758d4ed6416c22f609e8a371009c12283cbb3cdb
soultripper <sky@filmlovers.co.uk>
2014-04-25 23:35:38 +0700

    trying to get booking working

--- app/services/my_typhoeus_adapter.rb
+++ app/services/my_typhoeus_adapter.rb
@@ -2,6 +2,7 @@ class MyTyphoeusAdapter < Faraday::Adapter::Typhoeus
   def request(*)
     request = super
     request.options.merge!(:nosignal => true)
+    request.ssl_verifypeer = false
     request
   end
 end
